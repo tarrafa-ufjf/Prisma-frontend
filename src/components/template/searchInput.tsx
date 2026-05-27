9381eec521521ced8b933c89d8563f0ba3812a08
Alfredo Lucas <alfredoliu14@live.com>
2026-05-27 12:07:57 -0300

    feat: add internationalization support for Filters and update SearchInput placeholder

--- src/components/template/searchInput.tsx
+++ src/components/template/searchInput.tsx
@@ -1,12 +1,14 @@
 import { Search, X } from "lucide-react";
-import { CiSearch } from "react-icons/ci";
+import { useTranslations } from "next-intl";
 
 interface SearchInputProps {
     searchTerm: string;
     setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
     placeholder?: string;
 }
-const SearchInput = ({ searchTerm, setSearchTerm, placeholder = "Buscar" }: SearchInputProps) => {
+const SearchInput = ({ searchTerm, setSearchTerm, placeholder }: SearchInputProps) => {
+    const t = useTranslations("Filters");
+
     const clearSearch = () => {
         setSearchTerm('')
     }
@@ -14,7 +16,7 @@ const SearchInput = ({ searchTerm, setSearchTerm, placeholder = "Buscar" }: Sear
         <div className="ml-auto flex items-center gap-2 border-2 rounded-sm border-gray-300 px-4 py-2">
             {!searchTerm ? (<Search />) : (<X className="cursor-pointer" onClick={clearSearch} />)}
             <input
-                placeholder={placeholder}
+                placeholder={placeholder ?? t("search")}
                 className="w-48 focus-visible:outline-none"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
