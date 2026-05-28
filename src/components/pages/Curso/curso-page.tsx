11b5ee21bf966b22e7efe1a288ea0e35a3ab6d6e
Alfredo Lucas <alfredoliu14@live.com>
2026-05-28 10:45:12 -0300

    feat: add internationalization support for CursoPage component

--- src/components/pages/Curso/curso-page.tsx
+++ src/components/pages/Curso/curso-page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
 import Indicators from './Indicator/Indicators';
 import DadosGerais from './DadosGerais/DadosGerais';
 import Graficos from './Graficos/graficos';
@@ -11,13 +12,14 @@ type CursoPageProps = {
 };
 
 export default function CursoPage({ curso }: CursoPageProps) {
+  const t = useTranslations('Courses');
 
   return (
     <div className="flex-1 flex justify-center items-center pl-[240px]">
       <div className="BoxCurso">
         <div className="flex flex-row justify-between items-start w-full">
           <div className="flex flex-col items-start">
-            <h1 className="text-xl font-poppins font-semibold text-left">Disciplina</h1>
+            <h1 className="text-xl font-poppins font-semibold text-left">{t('course')}</h1>
             <p style={{ color: '#374DAA' }} className="text-left text-xl font-semibold">
               {curso.fullname}
             </p>
