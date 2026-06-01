8b9709b1116c68738952cb1e0045c8fe3ba8d319
Alfredo Lucas <alfredoliu14@live.com>
2026-06-01 09:43:04 -0300

    feat: add internationalization support for course not found message in details page

--- src/app/[locale]/(auth)/tutores/curso/[id_course]/details/page.tsx
+++ src/app/[locale]/(auth)/tutores/curso/[id_course]/details/page.tsx
@@ -1,6 +1,7 @@
 import Tutores from "@/components/pages/Tutores/tutores";
 import NotFound from "@/components/ui/not-found";
 import { getCourses } from "@/utils/api-server";
+import { getTranslations } from "next-intl/server";
 
 interface PageProps {
   params: Promise<{
@@ -11,6 +12,7 @@ interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const page_param = await params;
+  const t = await getTranslations("Courses");
   const cursos = await getCourses();
   const curso = cursos.filter((curso) => curso.id == Number(page_param.id_course))[0];
 
@@ -18,10 +20,7 @@ export default async function Page({ params }: PageProps) {
     return (
       <NotFound cursos={cursos}>
         <div className="flex-1 flex justify-center items-center pt-4 pl-[240px]">
-          <p>
-            Curso {page_param.id_course} não encontrado! Por favor, use o menu no canto
-            superior esquerdo, ou tente novamente mais tarde!
-          </p>
+          <p>{t("notFound", { courseId: page_param.id_course })}</p>
         </div>
       </NotFound>
     );
